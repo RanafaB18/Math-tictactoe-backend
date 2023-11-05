a9ef622ffdcae91b3a028a9cf106635ff814a1f8
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-11-05 22:19:56 +0000

    fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = createServer(app);
 const io = new Server(server, {
     cors: {
         // origin: "http://localhost:5173"
-        origin: "*"
+        origin: "https://math-tictactoe.vercel.app/"
     }
 })
 app.use(express.json())
